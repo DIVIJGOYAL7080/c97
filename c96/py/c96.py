ade35cb30c2ee71207d38199bfcc79577812d9a5
DIVIJGOYAL7080 <66262102+DIVIJGOYAL7080@users.noreply.github.com>
2021-09-20 19:55:40 +0530

    Add files via upload

---
+++ c96/py/c96.py
@@ -0,0 +1,33 @@
+'''age= int(input("enter ur age : "))
+if(age>=18):
+    print ("eligible to vote") 
+else : 
+    print ("ur not eligible to vote")'''
+
+
+#writing a program to check the pocket money
+'''pocketmoney= int(input("enter pocket money u get every month : "))
+if(pocketmoney>500):
+    print("ur rich")
+elif(pocketmoney>100):
+    print ("life is good")
+else :
+    print ("im broke")'''
+
+
+
+
+#write a program to count word
+'''word=input("enter a string ")
+charactercount=0
+wordcount=1
+for i in word:
+    charactercount=charactercount+1
+    if(i ==' '):
+        wordcount=wordcount+1
+print("no. of words in the string")
+print (wordcount)
+print("no. of character in the string")
+print (charactercount)'''
+
+
